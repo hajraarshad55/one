d7f0dd08ce373a6abbe3e8e06929aa9d47541237
hajraarshad55 <hajraarshad36@gmail.com>
2023-09-09 19:43:11 +0500

    aa

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,17 @@ const client = require("../connection");
 
 const rolesController = require("../controllers/rolesController/roles.js");
 const router = express.Router();
-
+const usersController = require("../controllers/usersController/users.js");
+//const router = express.Router();
 router.get("/getAllRoles", rolesController.getAllRoles);
 router.post("/postALLRoles", rolesController.postAllRoles);
-// router.put("/updateALLRoles/:id", rolesController.updateALLRoles);
-// router.get("/getRolesById/:id", rolesController.getRoleById);
+router.put("/updateALLRoles/:id", rolesController.updateAllRoles);
+router.get("/getRoleById/:id", rolesController.getRoleById);
+router.delete("/deleteRolesById/:id", rolesController.deleteRolesById);
 
+router.get("/getAllUsers", usersController.getAllUsers);
+router.post("/postALLUsers", usersController.postAllUsers);
+router.put("/updateALLUsers/:id", usersController.updateAllUsers);
+router.get("/getUsersById/:id", usersController.getUsersById);
+router.delete("/deleteUsersById/:id", usersController.deleteUsersById)
 module.exports = router;
